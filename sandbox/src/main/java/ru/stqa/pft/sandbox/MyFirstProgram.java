7b0dc893849e99f932bbf855ad469b878b3c178e
Nurlimbo <alimbaev.nurjigit@gmail.com>
2021-02-09 15:30:53 +0300

    functions

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,6 +2,23 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+      hello("23");
+      hello("user");
+
+      int a = 4;
+      System.out.println("Площадь кв " + a + " = "+ arik(a));
+
+      int l = 5;
+      int b = 5;
+      System.out.println("Площадь прям " + l +" и " + b + " = "+ aric(l, b));
+    }
+    public static void hello(String a) {
+      System.out.println("Hello, " + a + "!");
+    }
+    public static int arik(int a) {
+      return a*a;
+    }
+    public static int  aric(int l, int b) {
+      return l * b;
     }
 }
